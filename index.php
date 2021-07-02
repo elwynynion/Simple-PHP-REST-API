1ba7ad0109fe9d960ed7af3f8b0232ef63df5122
Elwyn Ynion <elwynynionpersonal@gmail.com>
2021-07-02 22:03:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -155,7 +155,7 @@ $(document).ready(function () {
     });
 });
 
-
+//
 window.addEventListener("pageshow", function (event) {
     var historyTraversal = event.persisted || (typeof window.performance != "undefined" && window.performance.navigation.type === 2);
     if (historyTraversal) {
